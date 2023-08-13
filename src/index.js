c8ab3b3d5314347ce213e98dcef23429a98e0f57
Ade Setiawan <adesetiawan0675@gmail.com>
2023-08-13 15:08:54 +0700

    add endpoint

--- src/index.js
+++ src/index.js
@@ -20,6 +20,22 @@ app.get("/products", async (req, res) => {
   res.send(products);
 });
 
+app.get("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  const products = await prisma.product.findUnique({
+    where: {
+      id: parseInt(productId)
+    }
+  });
+
+  if(!products){
+    return res.status(400).send({message:"Product not found!"});
+  }
+
+  res.send(products);
+});
+
 app.post("/products", async (req, res) => {
   const product = req.body;
 
@@ -33,8 +49,77 @@ app.post("/products", async (req, res) => {
   });
 
   res.status(201).send({
-    data: product,
+    data: products,
     message: "Create new product success",
+  });
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+  await prisma.product.delete({
+    where: {
+      id: parseInt(productId),
+    },
+  });
+
+  res.status(200).send({
+    message: "product delete success",
+  });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+  const productData = req.body;
+
+  if (
+    !(
+      productData.name &&
+      productData.price &&
+      productData.description &&
+      productData.image
+    )
+  ) {
+    res.send("some field missing");
+    return;
+  }
+
+  const products = await prisma.product.update({
+    where: {
+      id: parseInt(productId),
+    },
+    data: {
+      name: productData.name,
+      price: productData.price,
+      description: productData.description,
+      image: productData.name,
+    },
+  });
+
+  res.status(201).send({
+    data: products,
+    message: "update product success",
+  });
+
+  app.patch("/products/:id", async (req, res) => {
+    const productId = req.params.id;
+    const productData = req.body;
+
+    const products = await prisma.product.({
+      where: {
+        id: parseInt(productId),
+      },
+      data: {
+        name: productData.name,
+        price: productData.price,
+        description: productData.description,
+        image: productData.name,
+      },
+    });
+
+    res.status(201).send({
+      data: products,
+      message: "update product success",
+    });
   });
 });
 
